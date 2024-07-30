3d69d5c54b2dca26b8af78809b61721cd0e05e89
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-07-30 18:27:43 +0900

    login実装

--- server/src/app.py
+++ server/src/app.py
@@ -1,47 +1,75 @@
 import os
-from datetime import datetime
 
-from flask import Flask, render_template
-from flask_migrate import Migrate
+import psycopg2
+from flask import Flask, flash, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+DATABASE_URI = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}".format(
+    **{
+        "user": os.environ["POSTGRES_USER"],
+        "password": os.environ["POSTGRES_PASSWORD"],
+        "host": os.environ["HOST"],
+        "database_name": os.environ["DATABASE_NAME"],
+        "port": os.environ["DATABASE_PORT"],
+    }
+)
+
+
+def get_connection():
+    conn = psycopg2.connect(
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host=os.environ["HOST"],
+        database=os.environ["DATABASE_NAME"],
+        port=os.environ["DATABASE_PORT"],
+    )
+    return conn
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)  # 作成日時
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)  # 更新日時
 
 
 app = Flask(__name__)
-app.config.update(
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}".format(
-        **{
-            "user": os.environ["POSTGRES_USER"],
-            "password": os.environ["POSTGRES_PASSWORD"],
-            "host": os.environ["HOST"],
-            "database_name": os.environ["DATABASE_NAME"],
-            "port": os.environ["DATABASE_PORT"],
-        }
-    ),
-)
+app.config.update(SQLALCHEMY_TRACK_MODIFICATIONS=False, SQLALCHEMY_DATABASE_URI=DATABASE_URI, SECRET_KEY="key")
 db.init_app(app)
-Migrate(app, db)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def signin():
-    return "Hello World!"
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        query = f"SELECT * FROM users WHERE name = '{name}' AND password = '{password}'"
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+                user = cur.fetchone()
+                if user:
+                    return redirect("/mypage")
+                else:
+                    flash("Invalid username or password")
+                    return redirect("/")
+    else:
+        return render_template("signin.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        query = f"INSERT INTO users (name, password) VALUES ('{name}', '{password}')"
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+        return redirect("/")
+    else:
+        return render_template("signup.html")
 
 
 if __name__ == "__main__":
