294d1e34a25a6bbc9589df1e917a6755a436d8ef
Taisuké Nogami <ta-nogami@kddi.com>
2024-08-29 17:25:38 +0900

    修正

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ```
 hoge' OR 1=1; --
 ```
+
+```
+Password'); INSERT INTO users (name, password) VALUES ('Fake User', 'Fake Password'); --
+```
--- server/src/app.py
+++ server/src/app.py
@@ -5,14 +5,13 @@ from flask import Flask, flash, redirect, render_template, request
 
 
 def get_connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         user=os.environ["POSTGRES_USER"],
         password=os.environ["POSTGRES_PASSWORD"],
         host=os.environ["HOST"],
         database=os.environ["DATABASE_NAME"],
         port=os.environ["DATABASE_PORT"],
     )
-    return conn
 
 
 app = Flask(__name__)
@@ -22,21 +21,16 @@ app.secret_key = "secret"
 @app.route("/", methods=["GET", "POST"])
 def signin():
     if request.method == "POST":
-        name = request.form.get("name")
-        password = request.form.get("password")
-        with get_connection() as conn:
-            with conn.cursor() as cur:
-                # query = "SELECT * FROM users WHERE name = %s AND password = %s"
-                # cur.execute(query, (name, password))
-                query = f"SELECT * FROM users WHERE name = '{name}' AND password = '{password}'"
-                cur.execute(query)
-                user = cur.fetchone()
-                if user:
-                    flash("Login successful", "flash-success")
-                    return redirect("/")
-                else:
-                    flash("Invalid username or password", "flash-error")
-                    return redirect("/")
+        name, password = request.form["name"], request.form["password"]
+        with get_connection() as conn, conn.cursor() as cur:
+            # cur.execute("SELECT * FROM users WHERE name = %s AND password = %s", (name, password))
+            cur.execute(f"SELECT * FROM users WHERE name = '{name}' AND password = '{password}'")
+            user = cur.fetchone()
+            if user:
+                flash("Login successful", "flash-success")
+            else:
+                flash("Invalid username or password", "flash-error")
+        return redirect("/")
     else:
         return render_template("signin.html")
 
@@ -44,18 +38,13 @@ def signin():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        name = request.form.get("name")
-        password = request.form.get("password")
-        with get_connection() as conn:
-            with conn.cursor() as cur:
-                # query = "INSERT INTO users (name, password) VALUES (%s, %s)"
-                # cur.execute(query, (name, password))
-                query = f"INSERT INTO users (name, password) VALUES ('{name}', '{password}')"
-                cur.execute(query)
-                conn.commit()
+        name, password = request.form["name"], request.form["password"]
+        with get_connection() as conn, conn.cursor() as cur:
+            # cur.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (name, password))
+            cur.execute(f"INSERT INTO users (name, password) VALUES ('{name}', '{password}')")
+            conn.commit()
         return redirect("/")
-    else:
-        return render_template("signup.html")
+    return render_template("signup.html")
 
 
 @app.route("/mypage")
@@ -65,11 +54,10 @@ def mypage():
 
 @app.route("/admin")
 def admin():
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM users")
-            users = cur.fetchall()
-            return render_template("admin.html", users=users)
+    with get_connection() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM users")
+        users = cur.fetchall()
+    return render_template("admin.html", users=users)
 
 
 if __name__ == "__main__":
