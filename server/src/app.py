cc9fe31d2f814cf96a59e33094dda22b4e444679
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-09-16 21:03:47 +0900

    ormに書き換え

--- server/src/app.py
+++ server/src/app.py
@@ -1,35 +1,54 @@
 import os
 
-import psycopg2
 from flask import Flask, flash, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-def get_connection():
-    return psycopg2.connect(
-        user=os.environ["POSTGRES_USER"],
-        password=os.environ["POSTGRES_PASSWORD"],
-        host=os.environ["HOST"],
-        database=os.environ["DATABASE_NAME"],
-        port=os.environ["DATABASE_PORT"],
+def init_db(app: Flask) -> None:
+    db.init_app(app)
+
+
+def set_config(app: Flask) -> None:
+    app.config.update(
+        SECRET_KEY="dev",
+        WTF_CSRF_SECRET_KEY="csrf",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}".format(
+            **{
+                "user": os.environ["POSTGRES_USER"],
+                "password": os.environ["POSTGRES_PASSWORD"],
+                "host": os.environ["HOST"],
+                "database_name": os.environ["DATABASE_NAME"],
+                "port": os.environ["DATABASE_PORT"],
+            }
+        ),
     )
 
 
 app = Flask(__name__)
+set_config(app)
+init_db(app)
 app.secret_key = "secret"
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 
 @app.route("/", methods=["GET", "POST"])
 def signin():
     if request.method == "POST":
         name, password = request.form["name"], request.form["password"]
-        with get_connection() as conn, conn.cursor() as cur:
-            # cur.execute("SELECT * FROM users WHERE name = %s AND password = %s", (name, password))
-            cur.execute(f"SELECT * FROM users WHERE name = '{name}' AND password = '{password}'")
-            user = cur.fetchone()
-            if user:
-                return redirect("/mypage")
-            else:
-                flash("Invalid username or password", "flash-error")
+        user = User.query.filter_by(name=name, password=password).first()
+        if user:
+            return redirect("/mypage")
+        else:
+            flash("Invalid username or password", "flash-error")
         return redirect("/")
     else:
         return render_template("signin.html")
@@ -39,10 +58,9 @@ def signin():
 def signup():
     if request.method == "POST":
         name, password = request.form["name"], request.form["password"]
-        with get_connection() as conn, conn.cursor() as cur:
-            # cur.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (name, password))
-            cur.execute(f"INSERT INTO users (name, password) VALUES ('{name}', '{password}')")
-            conn.commit()
+        user = User(name=name, password=password)
+        db.session.add(user)
+        db.session.commit()
         return redirect("/")
     return render_template("signup.html")
 
@@ -54,11 +72,11 @@ def mypage():
 
 @app.route("/admin")
 def admin():
-    with get_connection() as conn, conn.cursor() as cur:
-        cur.execute("SELECT * FROM users")
-        users = cur.fetchall()
+    users = User.query.all()
     return render_template("admin.html", users=users)
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=80, debug=True)
